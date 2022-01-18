f014f5c71fd91e4ffc44b0a855469cfee8341475
muqingyou <53122181+muqingyou@users.noreply.github.com>
2022-01-18 23:46:45 +0800

    医生列表

--- miniprogram/pages/doctor/doctor.js
+++ miniprogram/pages/doctor/doctor.js
@@ -1,32 +1,122 @@
 // pages/doctor/doctor.js
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    page:1,
+    doctor_list:[
+      
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+   
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    // 请求医生数据
+    wx.cloud.callFunction({
+      name: 'cloud-doctor',
+      success: res => {
+        console.log(res.result.data)
+        this.setData({
+          doctor_list:res.result.data
+        })
+      },
+      fail: res => {
+        console.log("fail")
+        console.log(res)
+      },
+      complete:res=>{
+        console.log("complete")
+      }
+    })
 
+    // api.getRequestData('doctor_first_page/list',{},'GET',false).then((res)=>{
+    //   console.log("res_doctor---",res);
+    //   this.setData({ 
+    //     article_list:res.data.data
+    //   })
+    //   console.log(this.data.article_list)
+    // })
+    // wx.request({               
+    //   url:'http://127.0.0.1/get/consult/doctor_list?page='+this.data.page,
+    //   data:{
+    //     //categoryType:this.data.tabCur+1
+    //   },
+    //   success: (res) => {
+    //     this.setData({ 
+    //       doctor_list:res.data.data
+    //     })
+    //   }
+    // })  
+  },
+
+  // 跳转详细对话框
+  toDetail:function(){
+    // let index=e.currentTarget.dataset.index;
+    wx.navigateTo({
+      // url: '/pages/chat/chat?index='+index,
+      url:'/pages/chat/chat'
+    })
+  },
+
+  // 上拉刷新
+  scrolltolower() {
+    if (this.lock) {
+      wx.showToast({
+        title: '已经到底了！',
+        icon: 'none'
+      })
+      return
+    }
+    this.data.page++
+      wx.showLoading({
+        title: '正在加载中...',
+      })
+    //   wx.request({               
+    //     url:'http://127.0.0.1/get/consult/doctor_list?page='+this.data.page,
+    //     data:{
+    //       //categoryType:this.data.tabCur+1
+    //     },
+    //     success: (res) => {
+    //       var goodsList = this.data.doctor_list;
+    //       const newGoods = res.data.data
+    //       if (newGoods.length <= 0) {
+    //         //this.lock = true
+    //         wx.hideLoading()
+    //         wx.showToast({
+    //           title: '已经到底了！',
+    //           icon: 'none'
+    //         })
+    //         this.setData({
+    //           hasMore: false
+    //         })
+    //         return
+    //       }
+    //       wx.hideLoading()
+    //       goodsList = [...goodsList, ...newGoods]    //取数组里的一个值，然后再赋值到一个新数组里
+    //       this.setData({
+    //         doctor_list: goodsList
+    //       }) 
+    //   }
+    // })  
   },
 
   /**
