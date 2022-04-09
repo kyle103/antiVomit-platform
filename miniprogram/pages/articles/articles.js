7f6a1336d326b5f707a0953bd9fd049ef7378ae0
muqingyou <751535624@qq.com>
2022-04-10 00:02:43 +0800

    冲突合并

--- miniprogram/pages/articles/articles.js
+++ miniprogram/pages/articles/articles.js
@@ -5,12 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
+    articles:[],
+    page:0,
+    none:true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.showLoading({
+      title: '加载中',
+    })
     wx.cloud.callFunction({
       name: 'cloud-articles',
       data: {
@@ -19,7 +25,17 @@ Page({
       },
       success:res => {
         console.log('获取文章列表success',res)
-        
+        this.setData({
+          articles:res.result.data
+        })
+        wx.hideLoading({
+          success: (res) => {},
+        })
+      },
+      fail:err => {
+        wx.hideLoading({
+          success: (res) => {},
+        })
       }
     })
     
@@ -37,6 +53,67 @@ Page({
    */
   onShow: function () {
 
+  },
+
+  search:function(event){
+    wx.showLoading({
+      title: '搜索中',
+    })
+    wx.cloud.callFunction({
+      name: 'cloud-articles',
+      data: {
+        action: 'searchArticles',
+        keywords:event.detail
+      },
+      success:res => {
+        console.log('获取文章搜索结果success',res)
+        if(res.result.length===0){
+          this.setData({
+            none:false
+          })
+        }
+        this.setData({
+          articles:res.result
+        })
+        wx.hideLoading({
+          success: (res) => {},
+        })
+      },
+      fail:err => {
+        wx.hideLoading({
+          success: (res) => {},
+        })
+      }
+    })
+  },
+
+  cancelSearch:function(event){
+    this.setData({
+      none:true
+    })
+    this.onLoad()
+  },
+
+  // 跳转详细对话框
+  toDetail:function(e){
+    wx.navigateTo({
+      url: '/pages/articles/articleDetail/articleDetail?url='+ e.currentTarget.dataset.index,
+    })
+  },
+
+  // 上拉刷新
+  scrolltolower() {
+    if (this.lock) {
+      wx.showToast({
+        title: '已经到底了！',
+        icon: 'none'
+      })
+      return
+    }
+    this.data.page++
+      wx.showLoading({
+        title: '正在加载中...',
+      })
   },
 
   /**
